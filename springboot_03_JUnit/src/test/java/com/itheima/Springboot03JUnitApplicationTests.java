712fd3ed75b5018adb9db499e63f3fb96c810ce8
GavinGuo <122486975@qq.com>
2023-04-27 17:07:17 +0800

    chore: update

--- springboot_03_JUnit/src/test/java/com/itheima/Springboot03JUnitApplicationTests.java
+++ springboot_03_JUnit/src/test/java/com/itheima/Springboot03JUnitApplicationTests.java
@@ -1,11 +1,12 @@
-package com.itheima;
+package com;
 
+import com.itheima.Springboot03JUnitApplication;
 import com.itheima.dao.BookDao;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = Springboot03JUnitApplication.class)
 class Springboot03JUnitApplicationTests {
 	@Autowired
 	private BookDao bookDao;

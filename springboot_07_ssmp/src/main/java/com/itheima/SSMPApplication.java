0ec7fae5a275a1b201dd6cd6059e5a93c801da95
GavinGuo <122486975@qq.com>
2023-05-09 10:43:14 +0800

    feat: SSMP配置

--- springboot_07_ssmp/src/main/java/com/itheima/SSMPApplication.java
+++ springboot_07_ssmp/src/main/java/com/itheima/SSMPApplication.java
@@ -7,7 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SSMPApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SSMPApplication.class, args);
+//        // 这里就是临时配置的入参
+//        System.out.println(args);
+//        String[] arg = new String[1];
+//        arg[0] = "--server.port=8082";
+//        SpringApplication.run(SSMPApplication.class, arg);
+//        SpringApplication.run(SSMPApplication.class, args);
+        // 可以在启动Boot程序时断开读取外部临时配置对应的入口，也就是去掉读取外部参数的形参
+        SpringApplication.run(SSMPApplication.class);
     }
 
 }

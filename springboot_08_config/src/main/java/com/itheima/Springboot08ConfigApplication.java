b65ac4d5835bb1cb0c06c9d24f3489ef48134f33
GavinGuo <122486975@qq.com>
2023-05-09 14:44:14 +0800

    feat: 添加多环境配置项目模板

--- springboot_08_config/src/main/java/com/itheima/Springboot08ConfigApplication.java
+++ springboot_08_config/src/main/java/com/itheima/Springboot08ConfigApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Springboot08ConfigApplication {
-	static {}
 	public static void main(String[] args) {
 		SpringApplication.run(Springboot08ConfigApplication.class, args);
 	}
---
+++ springboot_09_profiles/src/main/java/com/itheima/Springboot09ProfilesApplication.java
@@ -0,0 +1,13 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot09ProfilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot09ProfilesApplication.class, args);
+    }
+
+}
